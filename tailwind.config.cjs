579f3eeb93465f7ab33c72c5e4b31257b4ce3406
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-21 19:56:16 +0800

    added custom padding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
       padding: {
         "header": "clamp(1rem, 0.76rem + 1.02vw, 1.25rem)",
+        "24px-80px": "clamp(1.5rem, -1.84rem + 14.25vw, 5rem)",
       },
       fontFamily: {
         "helvetica": ["Helvetica", "sans-serif"],
